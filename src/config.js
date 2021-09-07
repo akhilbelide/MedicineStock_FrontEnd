224bd71badd5b2fe07aa05ab9456cadecf56535f
akhil <akhil@akhils-MacBook-Air.local>
2021-09-07 21:38:19 +0530

    Changed hosturl to heroku

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const hosturl = 'https://touch-less-order.herokuapp.com' 
-export const hosturl = 'http://localhost:8080' 
+export const hosturl = 'https://medicine-stock-back-end.herokuapp.com' 
+//export const hosturl = 'http://localhost:8080' 
